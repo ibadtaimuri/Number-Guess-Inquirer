9956426d6bb006b96c8f58aee89bebb02a7e7033
Ibad Taimuri <ibad.taimuri@icloud.com>
2024-05-23 21:37:41 +0500

    CLI Based Number Guess using Inquirer
    Number Guess Game with track of how many attempts are made to guess the right number

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer"
+
+let randNum = Math.floor(Math.random() * 5) + 1; // Generate random number between 1 and 5 (inclusive)
+
+let numAttempts = 0; // Initialize attempt counter
+let keepPlaying = true;
+
+while (keepPlaying) {
+    let userGuess = await inquirer.prompt([
+        {
+            type: "number",
+            message: `Guess a Number between 1 & 5 (Attempt ${numAttempts + 1}): `,
+            name: "guess",
+        },
+    ]);
+
+    numAttempts++; // Update attempt counter
+
+    if (userGuess.guess === randNum) {
+        console.log(`You guessed the correct number in ${numAttempts} attempts!`);
+        keepPlaying = false; // Exit the loop on correct guess
+    } else {
+        console.log("Incorrect guess. Try again.");
+    }
+}
